865921d1beacf2596b4ae5c130cfede1104f55df
WannRiz <90831211+WannRiz@users.noreply.github.com>
2021-10-20 00:54:13 +0800

    Add files via upload

---
+++ message/allmenu.js
@@ -0,0 +1,567 @@
+const allmenu = (prefix) => {
+	return`
+\`\`\`Hai Kak👋
+
+❐──➨「 𝑰𝒏𝒇𝒐 𝒃𝒐𝒕 」
+𒍮Bot name : Wann Botz
+𒍮Owner : Wann Store
+
+┏⬡  *GROUP MENU*
+┃▹  ${prefix}groupsetting
+┃▹  ${prefix}antilink
+┃▹  ${prefix}antibitly
+┃▹  ${prefix}antivirtex
+┃▹  ${prefix}antihidetag
+┃▹  ${prefix}group
+┃▹  ${prefix}opentime
+┃▹  ${prefix}closetime
+┃▹  ${prefix}afk *ALASAN*
+┃▹  ${prefix}ceksewa
+┃▹  ${prefix}kickall
+┃▹  ${prefix}infogrup
+┃▹  ${prefix}promote
+┃▹  ${prefix}demote
+┃▹  ${prefix}listonline
+┃▹  ${prefix}tagall *TEKS*
+┃▹  ${prefix}leave
+┃▹  ${prefix}kick *REPLY*
+┃▹  ${prefix}add *62xxxx*
+┃▹  ${prefix}setgrupname
+┃▹  ${prefix}setppgrup
+┃▹  ${prefix}setdesc
+┃▹  ${prefix}sider *REPLY*
+┃▹  ${prefix}hidetag *REPLY/TEKS*
+┃▹  ${prefix}fitnah
+┗━━━━━━━━━━━━━━━━━━━━━⬡
+
+┏━▹ *Text Pro Me*
+┃▹ ${prefix}blackpink text
+┃▹ ${prefix}neon text
+┃▹ ${prefix}greenneon text
+┃▹ ${prefix}advanceglow text
+┃▹ ${prefix}futureneon text
+┃▹ ${prefix}sandwriting text
+┃▹ ${prefix}sandsummer text
+┃▹ ${prefix}sandengraved text
+┃▹ ${prefix}metaldark text
+┃▹ ${prefix}neonlight text
+┃▹ ${prefix}holographic text
+┃▹ ${prefix}text1917 text
+┃▹ ${prefix}minion text
+┃▹ ${prefix}deluxesilver text
+┃▹ ${prefix}newyearcard text
+┃▹ ${prefix}bloodfrosted text
+┃▹ ${prefix}halloween text
+┃▹ ${prefix}jokerlogo text
+┃▹ ${prefix}fireworksparkle text
+┃▹ ${prefix}natureleaves text
+┃▹ ${prefix}bokeh text
+┃▹ ${prefix}toxic text
+┃▹ ${prefix}strawberry text
+┃▹ ${prefix}box3d text
+┃▹ ${prefix}roadwarning text
+┃▹ ${prefix}breakwall text
+┃▹ ${prefix}icecold text
+┃▹ ${prefix}luxury text
+┃▹ ${prefix}cloud text
+┃▹ ${prefix}summersand text
+┃▹ ${prefix}horrorblood text
+┃▹ ${prefix}thunder text
+┃▹ ${prefix}pornhub text1 text2
+┃▹ ${prefix}glitch text1 text2
+┃▹ ${prefix}avenger text1 text2
+┃▹ ${prefix}space text1 text2
+┃▹ ${prefix}ninjalogo text1 text2
+┃▹ ${prefix}marvelstudio text1 text2
+┃▹ ${prefix}lionlogo text1 text2
+┃▹ ${prefix}wolflogo text1 text2
+┃▹ ${prefix}steel3d text1 text2
+┃▹ ${prefix}wallgravity text1 text2
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏━▹ *NSFW MENU*
+┃▹ ${prefix}chiisaihentai
+┃▹ ${prefix}trap
+┃▹ ${prefix}blowjob
+┃▹ ${prefix}yaoi
+┃▹ ${prefix}ecchi
+┃▹ ${prefix}hentai
+┃▹ ${prefix}ahegao
+┃▹ ${prefix}hololewd
+┃▹ ${prefix}sideoppai
+┃▹ ${prefix}animefeets
+┃▹ ${prefix}animebooty
+┃▹ ${prefix}animethighss
+┃▹ ${prefix}hentaiparadise
+┃▹ ${prefix}animearmpits
+┃▹ ${prefix}hentaifemdom
+┃▹ ${prefix}lewdanimegirls
+┃▹ ${prefix}biganimetiddies
+┃▹ ${prefix}animebellybutton
+┃▹ ${prefix}hentai4everyone
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏━▹ *RANDOM IMAGE*
+┃▹ ${prefix}bj
+┃▹ ${prefix}ero
+┃▹ ${prefix}ppcp
+┃▹ ${prefix}cum
+┃▹ ${prefix}feet
+┃▹ ${prefix}yuri
+┃▹ ${prefix}trap
+┃▹ ${prefix}lewd
+┃▹ ${prefix}feed
+┃▹ ${prefix}eron
+┃▹ ${prefix}solo
+┃▹ ${prefix}gasm
+┃▹ ${prefix}poke
+┃▹ ${prefix}anal
+┃▹ ${prefix}holo
+┃▹ ${prefix}tits
+┃▹ ${prefix}kuni
+┃▹ ${prefix}kiss
+┃▹ ${prefix}erok
+┃▹ ${prefix}smug
+┃▹ ${prefix}baka
+┃▹ ${prefix}solog
+┃▹ ${prefix}feetg
+┃▹ ${prefix}lewdk
+┃▹ ${prefix}waifu
+┃▹ ${prefix}pussy
+┃▹ ${prefix}femdom
+┃▹ ${prefix}cuddle
+┃▹ ${prefix}hentai
+┃▹ ${prefix}eroyuri
+┃▹ ${prefix}cum_jpg
+┃▹ ${prefix}blowjob
+┃▹ ${prefix}erofeet
+┃▹ ${prefix}holoero
+┃▹ ${prefix}classic
+┃▹ ${prefix}erokemo
+┃▹ ${prefix}fox_girl
+┃▹ ${prefix}futanari
+┃▹ ${prefix}lewdkemo
+┃▹ ${prefix}wallpaper
+┃▹ ${prefix}pussy_jpg
+┃▹ ${prefix}kemonomimi
+┃▹ ${prefix}nsfw_avatar
+┃▹ ${prefix}ngif
+┃▹ ${prefix}nsfw_neko_gif
+┃▹ ${prefix}random_hentai_gif
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏━▹ *PHOTO OXY*
+┃▹ ${prefix}shadow text
+┃▹ ${prefix}cup text
+┃▹ ${prefix}cup1 text
+┃▹ ${prefix}romance text
+┃▹ ${prefix}smoke text
+┃▹ ${prefix}burnpaper text
+┃▹ ${prefix}lovemessage text
+┃▹ ${prefix}undergrass text
+┃▹ ${prefix}love text
+┃▹ ${prefix}coffe text
+┃▹ ${prefix}woodheart text
+┃▹ ${prefix}woodenboard text
+┃▹ ${prefix}summer3d text
+┃▹ ${prefix}wolfmetal text
+┃▹ ${prefix}nature3d text
+┃▹ ${prefix}underwater text
+┃▹ ${prefix}golderrose text
+┃▹ ${prefix}summernature text
+┃▹ ${prefix}letterleaves text
+┃▹ ${prefix}glowingneon text
+┃▹ ${prefix}fallleaves text
+┃▹ ${prefix}flamming text
+┃▹ ${prefix}harrypotter text
+┃▹ ${prefix}carvedwood text
+┃▹ ${prefix}tiktok text1 text2
+┃▹ ${prefix}arcade8bit text1 text2
+┃▹ ${prefix}battlefield4 text1 text2
+┃▹ ${prefix}pubg text1 text2
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏━▹ *Ephoto 360*
+┃▹ ${prefix}wetglass text
+┃▹ ${prefix}multicolor3d text
+┃▹ ${prefix}watercolor text
+┃▹ ${prefix}luxurygold text
+┃▹ ${prefix}galaxywallpaper text
+┃▹ ${prefix}lighttext text
+┃▹ ${prefix}beautifulflower text
+┃▹ ${prefix}puppycute text
+┃▹ ${prefix}royaltext text
+┃▹ ${prefix}heartshaped text
+┃▹ ${prefix}birthdaycake text
+┃▹ ${prefix}galaxystyle text
+┃▹ ${prefix}hologram3d text
+┃▹ ${prefix}greenneon text
+┃▹ ${prefix}glossychrome text
+┃▹ ${prefix}greenbush text
+┃▹ ${prefix}metallogo text
+┃▹ ${prefix}noeltext text
+┃▹ ${prefix}glittergold text
+┃▹ ${prefix}textcake text
+┃▹ ${prefix}starsnight text
+┃▹ ${prefix}wooden3d text
+┃▹ ${prefix}textbyname text
+┃▹ ${prefix}writegalacy text
+┃▹ ${prefix}galaxybat text
+┃▹ ${prefix}snow3d text
+┃▹ ${prefix}birthdayday text
+┃▹ ${prefix}goldplaybutton text
+┃▹ ${prefix}silverplaybutton text
+┃▹ ${prefix}freefire text
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏━▹ IMAGE MANIPULATION
+┃▹ ${prefix}darkjoke
+┃▹ ${prefix}meme
+┃▹ ${prefix}joke
+┃▹ ${prefix}wasted
+┃▹ ${prefix}hitler
+┃▹ ${prefix}wanted
+┃▹ ${prefix}greyscale
+┃▹ ${prefix}trash
+┃▹ ${prefix}circle
+┃▹ ${prefix}blur
+┃▹ ${prefix}tinyurl
+┃▹ ${prefix}cuttly
+┃▹ ${prefix}affect
+┃▹ ${prefix}picture
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *PRIMBON MENU*
+┃▹ ${prefix}artinama *nama kamu*
+┃▹ ${prefix}artimimpi
+┃▹ ${prefix}jodoh
+┃▹ ${prefix}tebakumur *nama kamu*
+┃▹ ${prefix}tebakgender *nama kamu*
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *STORY MENU*
+┃▹ ${prefix}drakorongoing
+┃▹ ${prefix}lk21 query
+┃▹ ${prefix}wattpad url_wattpad
+┃▹ ${prefix}wattpadsearch query
+┃▹ ${prefix}cerpen
+┃▹ ${prefix}ceritahoror
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *RANDOM TEXT*
+┃▹ ${prefix}quotes
+┃▹ ${prefix}quotesdilan
+┃▹ ${prefix}quotesanime
+┃▹ ${prefix}quotesimage
+┃▹ ${prefix}faktaunik
+┃▹ ${prefix}katabijak
+┃▹ ${prefix}pantun
+┃▹ ${prefix}bucin
+┃▹ ${prefix}randomnama
+┃▹ ${prefix}darkjokes
+┃▹ ${prefix}memeindo
+┃▹ ${prefix}katailham
+┃▹ ${prefix}katajago
+┃▹ ${prefix}cekbapak
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *OWNER MENU*
+┃▹  ${prefix}antidelete
+┃▹  ${prefix}anticall
+┃▹  ${prefix}antivo
+┃▹  ${prefix}bc *teks*
+┃▹  ${prefix}term
+┃▹  ${prefix}eval
+┃▹  ${prefix}reset
+┃▹  ${prefix}clearall
+┃▹  ${prefix}leaveall
+┃▹  ${prefix}upswteks
+┃▹  ${prefix}upswimage
+┃▹  ${prefix}upswsticker
+┃▹  ${prefix}upswgif
+┃▹  ${prefix}upswaudio
+┃▹  ${prefix}upswvoice
+┃▹  ${prefix}upswvideo
+┃▹  ${prefix}leaveall
+┃▹  ${prefix}join *link gc*
+┃▹  ${prefix}shutdown
+┃▹  ${prefix}getquoted
+┃▹  ${prefix}addupdate *fiturnya*
+┃▹  ${prefix}exif *nama|author*
+┃▹  ${prefix}sewa add/del *waktunya*
+┃▹  ${prefix}premium add @tag|nomor
+┃▹  ${prefix}premium del @tag|nomor
+┃▹  ${prefix}hacked *TEKSNYA*
+┃▹  ${prefix}boomtext *TEKSNYA*
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *WIBU MENU*
+┃▹  ${prefix}loli
+┃▹  ${prefix}manga
+┃▹  ${prefix}anime 
+┃▹  ${prefix}lolivideo
+┃▹  ${prefix}husbu
+┃▹  ${prefix}waifu
+┃▹  ${prefix}milf
+┃▹  ${prefix}neko
+┃▹  ${prefix}kanna
+┃▹  ${prefix}sagiri
+┃▹  ${prefix}hentai
+┃▹  ${prefix}cosplay
+┃▹  ${prefix}wallnime
+┃▹  ${prefix}kusonime
+┃▹  ${prefix}megumin
+┃▹  ${prefix}otakudesu
+┃▹  ${prefix}otaku
+┃▹  ${prefix}doujindesu
+┃▹  ${prefix}storyanime
+┃▹  ${prefix}nakanomiku
+┃▹  ${prefix}nakanoikyy
+┃▹  ${prefix}nakanoitsuki
+┃▹  ${prefix}otakuongoing
+┃▹  ${prefix}nhentai *code*
+┃▹  ${prefix}nekopoi *link*
+┃▹  ${prefix}nekopoi3d
+┃▹  ${prefix}nekopoicosplay
+┃▹  ${prefix}nekopoisearch
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *DOWNLOAD MENU*
+┃▹  ${prefix}fb 
+┃▹  ${prefix}igdl 
+┃▹  ${prefix}igdl2 
+┃▹  ${prefix}twitter 
+┃▹  ${prefix}tiktok 
+┃▹  ${prefix}play 
+┃▹  ${prefix}ythd 
+┃▹  ${prefix}ytmp3 
+┃▹  ${prefix}ytmp4 
+┃▹  ${prefix}soundcloud 
+┃▹  ${prefix}tiktoknowm 
+┃▹  ${prefix}tiktokaudio
+┃▹  ${prefix}mediafire 
+┃▹  ${prefix}nhentaipdf *code*
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *OTHER MENU*
+┃▹  ${prefix}brainly *query*
+┃▹  ${prefix}shopee *product*
+┃▹  ${prefix}playstore *query*
+┃▹  ${prefix}ssweb *query*
+┃▹  ${prefix}google *query*
+┃▹  ${prefix}image *query*
+┃▹  ${prefix}pinterest *query*
+┃▹  ${prefix}nulis *teks*
+┃▹  ${prefix}nulis *teks*
+┃▹  ${prefix}iguser *ussername*
+┃▹  ${prefix}igstalk *username*
+┃▹  ${prefix}githubstalk *username*
+┃▹  ${prefix}tiktokstalk *ussername*
+┃▹  ${prefix}img2url *reply foto*
+┃▹  ${prefix}ytsearch *query*
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *GAME MENU*
+┃▹  ${prefix}slot
+┃▹  ${prefix}limitgame
+┃▹  ${prefix}gelud @tag
+┃▹  ${prefix}tictactoe @tag
+┃▹  ${prefix}siapaaku
+┃▹  ${prefix}family100
+┃▹  ${prefix}kuismath
+┃▹  ${prefix}asahotak
+┃▹  ${prefix}tebaklirik
+┃▹  ${prefix}tebaklagu
+┃▹  ${prefix}tebakkata
+┃▹  ${prefix}susunkata
+┃▹  ${prefix}kimiakuis
+┃▹  ${prefix}caklontong
+┃▹  ${prefix}tebakjenaka
+┃▹  ${prefix}tebakanime
+┃▹  ${prefix}tebaktebakan
+┃▹  ${prefix}tebakgambar
+┃▹  ${prefix}tebakbendera
+┃▹  ${prefix}suit *batu/kertas/gunting*
+┃▹  ${prefix}suit2
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *STICKER MENU*
+┃▹  ${prefix}dadu
+┃▹  ${prefix}amongus *TEKS*
+┃▹  ${prefix}doge
+┃▹  ${prefix}toimg
+┃▹  ${prefix}patrick
+┃▹  ${prefix}garwgura
+┃▹  ${prefix}ttg *teks*
+┃▹  ${prefix}ttp *teks
+┃▹  ${prefix}ttp2 *teks*
+┃▹  ${prefix}ttp3 *teks*
+┃▹  ${prefix}ttp4 *teks*
+┃▹  ${prefix}attp *teks*
+┃▹  ${prefix}stickeranime
+┃▹  ${prefix}semoji *emoji*
+┃▹  ${prefix}sticker *reply foto/video*
+┃▹  ${prefix}smeme *teks|teks*
+┃▹  ${prefix}swm *pack|author*
+┃▹  ${prefix}take *pack|author* 
+┃▹  ${prefix}tovideo *reply sgif*
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *FUN MENU*
+┃▹  ${prefix}mining
+┃▹  ${prefix}togel
+┃▹  ${prefix}cekwatak
+┃▹  ${prefix}cekmati [nama]
+┃▹  ${prefix}wangy [nama]
+┃▹  ${prefix}citacita
+┃▹  ${prefix}toxic
+┃▹  ${prefix}tod
+┃▹  ${prefix}truth
+┃▹  ${prefix}dare
+┃▹  ${prefix}apakah
+┃▹  ${prefix}bisakah
+┃▹  ${prefix}kapankah
+┃▹  ${prefix}rate
+┃▹  ${prefix}jadian
+┃▹  ${prefix}cantik
+┃▹  ${prefix}ganteng
+┃▹  ${prefix}beban
+┃▹  ${prefix}babi
+┃▹  ${prefix}ngentot
+┃▹  ${prefix}gantengcek
+┃▹  ${prefix}cantikcek
+┃▹  ${prefix}goblokcek
+┃▹  ${prefix}gaycek
+┃▹  ${prefix}sangecek
+┃▹  ${prefix}cekbapack
+┃▹  ${prefix}hbd
+┃▹  ${prefix}hilih *teks*
+┃▹  ${prefix}alay
+┃▹  ${prefix}hbk
+┃▹  ${prefix}bpurba
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *INFO MENU*
+┃▹  ${prefix}infocorona
+┃▹  ${prefix}cekkalender
+┃▹  ${prefix}cekramadhan
+┃▹  ${prefix}update
+┃▹  ${prefix}level
+┃▹  ${prefix}rules
+┃▹  ${prefix}profile
+┃▹  ${prefix}waktu
+┃▹  ${prefix}botstat
+┃▹  ${prefix}sewabot
+┃▹  ${prefix}listsewa
+┃▹  ${prefix}owner
+┃▹  ${prefix}ping
+┃▹  ${prefix}runtime
+┃▹  ${prefix}donasi
+┃▹  ${prefix}leaderboard
+┃▹  ${prefix}cekpremium
+┃▹  ${prefix}listpremium
+┃▹  ${prefix}sourcecode
+┃▹  ${prefix}bugreport *keluhan*
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡ *BOKEP MENU*
+┃▹ ${prefix}bokep1
+┃▹ ${prefix}bokep2
+┃▹ ${prefix}bokep3
+┃▹ ${prefix}bokep4
+┃▹ ${prefix}bokep5
+┃▹ ${prefix}bokep6
+┃▹ ${prefix}bokep7
+┃▹ ${prefix}bokep8
+┃▹ ${prefix}bokep9
+┃▹ ${prefix}bokep10
+┃▹ ${prefix}bokep11
+┃▹ ${prefix}bokep12
+┃▹ ${prefix}bokep13
+┃▹ ${prefix}bokep14
+┃▹ ${prefix}bokep15
+┃▹ ${prefix}bokep16
+┃▹ ${prefix}bokep17
+┃▹ ${prefix}bokep18
+┃▹ ${prefix}bokep19
+┃▹ ${prefix}bokep20
+┃▹ ${prefix}bokep21
+┃▹ ${prefix}bokep22
+┃▹ ${prefix}bokep23
+┃▹ ${prefix}bokep24
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *SOUND MENU*
+┃▹  ${prefix}sound1
+┃▹  ${prefix}sound2
+┃▹  ${prefix}sound3
+┃▹  ${prefix}sound4
+┃▹  ${prefix}sound5
+┃▹  ${prefix}sound6
+┃▹  ${prefix}sound7
+┃▹  ${prefix}sound8
+┃▹  ${prefix}sound9
+┃▹  ${prefix}sound10
+┃▹  ${prefix}sound11
+┃▹  ${prefix}sound12
+┃▹  ${prefix}sound13
+┃▹  ${prefix}sound14
+┃▹  ${prefix}sound15
+┃▹  ${prefix}sound16
+┃▹  ${prefix}sound17
+┃▹  ${prefix}sound18
+┃▹  ${prefix}sound19
+┃▹  ${prefix}sound20
+┃▹  ${prefix}sound21
+┃▹  ${prefix}sound22
+┃▹  ${prefix}sound23
+┃▹  ${prefix}sound24
+┃▹  ${prefix}sound25
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *ISLAM MENU*
+┃▹  ${prefix}listsurah
+┃▹  ${prefix}alquran
+┃▹  ${prefix}alquranaudio
+┃▹  ${prefix}asmaulhusna
+┃▹  ${prefix}kisahnabi
+┃▹  ${prefix}jadwalsholat
+┃▹  ${prefix}ayatkursi
+┃▹  ${prefix}bacaanshalat
+┃▹  ${prefix}doaharian
+┗━━━━━━━━━━━━━━━━━━━━⬡
+
+┏⬡  *STORAGE MENU*
+┃▹  ${prefix}slow
+┃▹  ${prefix}tupai
+┃▹  ${prefix}bass
+┃▹  ${prefix}robot
+┃▹  ${prefix}balik
+┃▹  ${prefix}gemuk
+┃▹  ${prefix}fast
+┃▹  ${prefix}imut
+┃▹  ${prefix}hode
+┃▹  ${prefix}nightcore
+┃▹  ${prefix}tts *KODE BAHASA*
+┃▹  ${prefix}addvn
+┃▹  ${prefix}getvn
+┃▹  ${prefix}listvn
+┃▹  ${prefix}addsticker
+┃▹  ${prefix}getsticker
+┃▹  ${prefix}liststicker
+┃▹  ${prefix}addimage
+┃▹  ${prefix}getimage
+┃▹  ${prefix}listimage
+┃▹  ${prefix}addvideo
+┃▹  ${prefix}getvideo
+┃▹  ${prefix}listvideo
+┃▹  ${prefix}addcmd
+┃▹  ${prefix}delcmd
+┃▹  ${prefix}listcmd
+┗━━━━━━━━━━━━━━━━━━━━⬡
+☀︎››› *Telp/Vc = Block*
+☀︎››› *Spam + Telpon = Banned*
+
+*━━━「 WannBotz 」━━━*	
+	`}
+	exports.allmenu = allmenu
