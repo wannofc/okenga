6179b823ed8dee7481ad629869f8062de9eb539b
WannRiz <90831211+WannRiz@users.noreply.github.com>
2021-10-19 21:45:06 +0800

    Add files via upload

---
+++ message/allmenu.js
@@ -0,0 +1,205 @@
+const allmenu = (prefix) => {
+	return`
+\`\`\`Hai Kak👋
+
+❐──➨「 𝑰𝒏𝒇𝒐 𝒃𝒐𝒕 」
+𒍮Bot name : 𝐾𝑢𝑛𝑧𝐵𝑜𝑡𝑧
+𒍮Owner : 𝐾𝑢𝑛𝑧𝑥𝐷
+𒍮Group Chat : ${groups.length}
+𒍮Private Chat : ${privat.length}
+𒍮Total chat : ${totalChat.length}
+
+❐──➨「 𝑰𝒏𝒇𝒐 𝒎𝒆𝒏𝒖 」
++🔖 cekprefix
++🔖 #stast
++🔖 #allmenu
++🔖 #limit
++🔖 #glimit
++🔖 #buylimit <angka>
++🔖 #buyglimit <angka>
++🔖 #balance
++🔖 #runtime
++🔖 #speed
++🔖 #owner
++🔖 #donasi
++🔖 #rules
++🔖 #report <bug nya>
++🔖 #sourcecode
++🔖 #listpremium
++🔖 #tagme
++🔖 #listban
++🔖 #listbot
++🔖 #snk
++
+❐──➨「 𝑻𝒐𝒐𝒍𝒔 」
++🔖 #sticker
++🔖 #swm
++🔖 #take
++🔖 #kontak nomor|nama
++🔖 #toimg
++🔖 #attp text
++🔖 #amongus text
++🔖 #codetext text
++🔖 #tinyurl url
++🔖 #nuliskiri text
++🔖 #nuliskanan text
++🔖 #foliokiri text
++🔖 #foliokanan text
++🔖 #translate kodebahasa teks/reply
++
+❐──➨「 𝑮𝒓𝒖𝒑 」
++🔖 #afk
++🔖 #infogrup
++🔖 #chatinfo
++🔖 #add 628xx
++🔖 #kick @tag
++🔖 #promote @tag
++🔖 #demote @tag
++🔖 #linkgc
++🔖 #leave
++🔖 #setdesc
++🔖 #setgrupname
++🔖 #setppgrup
++🔖 #opengrup
++🔖 #closegrup
++🔖 #join
++🔖 #tagall
++🔖 #hidetag
++🔖 #tagall
++🔖 #mute
++🔖 #unmute
++🔖 #antilink
++🔖 #welcome
++🔖 #left
++🔖 #antibadword
++🔖 #listbadword
++🔖 #addbadword
++🔖 #delbadword
++ 
+❐──➨「 𝑫𝒐𝒘𝒏𝒍𝒐𝒂𝒅 」
++🔖 #ytmp4 url
++🔖 #ytmp3 url
++🔖 #igdl url
++🔖 #fbdl url
++🔖 #tiktok url
++🔖 #yts query
++🔖 #play query
++🔖 #playmp4 query
++🔖 #igstalk username
++🔖 #ghstalk username
++ 
+❐──➨「 𝑺𝒕𝒊𝒄𝒌 𝒄𝒎𝒅 」
++🔖 #setcmd
++🔖 #delcmd
++
+❐──➨「 𝑴𝒂𝒌𝒆𝒓 」
++🔖 #blackpink text
++🔖 #tahta text
++🔖 #neon text
++🔖 #glitch text1|text2
++🔖 #thundername text
++🔖 #pornhub text1|text2
++🔖 #freefire text
++🔖 #horor text 
++🔖 #joker text
++🔖 #magma text
++🔖 #grafity text
++
+❐──➨「 𝑴𝒖𝒍𝒕𝒊 𝒔𝒆𝒔𝒔𝒊𝒐𝒏 」
++🔖 #jadibot
++🔖 #getcode
++🔖 #stopjadibot
++
+❐──➨「 𝑭𝒖𝒏 𝒎𝒆𝒏𝒖 」
++🔖 #jadian
++🔖 #ganteng
++🔖 #cantik
++🔖 #sercpatnert
++🔖 #tictactoe @tag
++🔖 #tebakgambar
++🔖 #family100
++🔖 #suit
++🔖 #apakah
++🔖 #bisakah
++🔖 #kapankah
++🔖 #hobby
++🔖 #rate
++🔖 #cekbapak
++🔖 #seberapagay
++🔖 #truth
++🔖 #dare
++
+❐──➨「 18+ 𝒎𝒆𝒏𝒖 」
++🔖 #kodenuklir
++🔖 #linkbokep
++🔖 #nekopoi1
++🔖 #nekopoi2
++🔖 #nekopoi4
++🔖 #nekopoi5
++🔖 #nekopoi6
++🔖 #nekopoi7
++🔖 #nekopoi8
++🔖 #nekopoi9
++🔖 #randombokep
++
+❐──➨「 𝑺𝒐𝒖𝒏𝒅 𝒎𝒆𝒏𝒖 」
++🔖 #sound
++🔖 #sound1
++🔖 #sound2
++🔖 #sound3
++🔖 #sound4
++🔖 #sound5
++🔖 #sound6
++🔖 #sound7
++🔖 #sound8
++🔖 #sound9
++🔖 #sound10
++🔖 #sound11
++🔖 #sound12
++🔖 #sound13
++🔖 #sound14
++🔖 #sound15
++🔖 #sound16
++🔖 #sound17
++🔖 #sound18
++🔖 #sound19
++🔖 #sound20
++
+❐──➨「 𝑴𝒆𝒏𝒈 𝒘𝒊𝒃𝒖 」
++🔖 #waifu
++🔖 #loli
++🔖 #nekonime
++🔖 #megumin
++🔖 #sagiri
++🔖 #shinobu
++ 
+❐──➨「 𝑶𝒘𝒏𝒆𝒓 」
++🔖 > eval
++🔖 $ execot
++🔖 #self
++🔖 #public
++🔖 #restart
++🔖 #off
++🔖 #resetlimit
++🔖 #setpp
++🔖 #setname
++🔖 #setbio
++🔖 #setprefix
++🔖 #bc
++🔖 #block
++🔖 #unblock
++🔖 #ban
++🔖 #unban
++🔖 #clearall
++🔖 #exif nama|author
++🔖 #addsewa link waktu
++🔖 #delsewa idgroup
++🔖 #addprem
++🔖 #delprem\`\`\`
+
+☀︎››› *Telp/Vc = Block*
+☀︎››› *Spam + Telpon = Banned*
+
+*━━━「 𝐾𝑢𝑛𝑧𝐵𝑜𝑡𝑧 」━━━*	
+	`}
+	exports.allmenu = allmenu
