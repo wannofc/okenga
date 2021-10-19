e9683149c2f00b5ef0d475a9692e146c9adaf497
WannRiz <90831211+WannRiz@users.noreply.github.com>
2021-10-19 15:43:35 +0800

    Add files via upload

---
+++ shop/price.js
@@ -0,0 +1,21 @@
+const list_price = (prefix) => {
+return `🎮 LIST GAME :
+
+${prefix}pubg -> List Uc Pubg
+${prefix}ml -> List Diamond MLBB
+${prefix}ff -> List  Diamond FF
+${prefix}cod -> List Cp Codm
+${prefix}aov -> List Voucher Aov
+${prefix}sausage -> List Candy Sausage
+${prefix}lol -> List Wc Lol
+${prefix}valo -> List Vp Valorant
+
+Untuk melihat listnya ketik ${prefix}list game 
+Ex :
+•> ${prefix}aov ( .aov )
+	ㅁ Untuk melihat list Voucher Aov 
+`
+	}
+
+exports.list_price = list_price
+ 
